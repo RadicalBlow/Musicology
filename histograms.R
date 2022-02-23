22fc78a5f338779d4bc6698590438f3e6c55cb8c
RadicalBlow <shankarrai2000@live.nl>
2022-02-23 18:00:25 +0100

    Transform index.rmd

--- histograms.R
+++ histograms.R
@@ -7,6 +7,7 @@ westernPop <- westernPop[!is.na(westernPop$tempo), ][1:100,]
 koreanPop <- get_playlist_audio_features("", "30EtqO7XgA36lwcdj1Uuex")
 allPop <- rbind(westernPop, koreanPop)
 
+test <- get_playlist_audio_features("", "45EFycwtfRhnDPWbc2mClK")
 ggplot(allPop, aes(x=energy)) +
   geom_histogram(bins=20) +
   facet_wrap(~ playlist_name)
